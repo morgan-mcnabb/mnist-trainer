a38bace31a6b9533cbde8979d2fbf4871b97eab7
morgan-mcnabb <morgan.mcnabb@protonmail.com>
2024-12-30 22:26:05 -0500

    added packages, config struct created

---
+++ src/config.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+    pub epochs: usize,
+    pub learning_rate: f32,
+    pub layers: Vec<usize>,
+    pub activations: Vec<String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            epochs: 20,
+            learning_rate: 0.1,
+            layers: vec![784, 128, 64, 10],
+            activations: vec!["sigmoid".to_string(), "sigmoid".to_string(), "sigmoid".to_string()],
+        }
+    }
+}
